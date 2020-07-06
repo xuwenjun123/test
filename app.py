74e47b27bb83e5152ea5af1e4725abff11e7a2ed
xuwenjun <1069034048@qq.com>
2020-07-06 21:00:30 +0800

    添加了首页模版

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
